a489f9465a79a11ec8cd982900e587d333d4b941
jayashreebaarathod-sys <jayashreebaarathod@gmail.com>
2026-01-07 05:03:43 -0500

    Add files via upload

---
+++ paython/excercise/list.py
@@ -0,0 +1,8 @@
+#create  a list to store your favourite destination in india.
+state=["goa","jaipur","kerala","ladakh","agra"]
+#display the list
+print(state)
+print("my favourite destination in india are:")
+print("position of goa",state.index('goa'))
+#print("position of goa ",states.index('jaipur'))#value not found i error
+print("count of goa ",state.count('goa'))#1
